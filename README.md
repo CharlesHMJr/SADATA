d72bd9d354cbe3adfd82e0787cf5cf4a8a9d5e1c
Charles Junior <acad.charlesjr@gmail.com>
2023-09-10 20:24:40 -0300

    Add heavy usage of csv module
    Seeking efficiency, the main pandas dataframes were removed. Now, data insertion occurs directly into the file

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 # 09/08/2023
 
 - [x] Update nascimento.csv: include city, state and country 
-- [ ] Create a checkpoint
+- ~~[ ] Create a checkpoint~~
 - [x] New formacao_completa.csv: includes complete history (course, start, end, place)
 
 _search about clustering_
@@ -11,7 +11,7 @@ _search about clustering_
 # 30/08/2023
 
 - [ ] Uptade localNascimento.csv: include professional address
-- [ ] Sumarizar a quantidade anual por cada curso
+- [ ] New contagemHistoricos.csv: summarize the annual amount for each course
 
 _search about 'Open Refine'_
 
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # %%
-
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import glob
+import csv
 import io
 import zipfile
-
-import glob
-import pandas as pd
-from lxml import etree
-from geopy.geocoders import Nominatim
 import folium
+
+import pandas as pd
+
+from geopy.geocoders import Nominatim
+from lxml import etree
 from folium.plugins import HeatMap
+
 # %%
 #for testing
 inicio = int(input('Informe o início: '))
@@ -19,24 +20,32 @@ fim = int(input('Informe o fim: '))
 curriculos = glob.glob('collection/*/*')
 cont = 0
 
-#creates a dataframe to store indentifiers and birth places
-localNascimento = pd.DataFrame(columns=['Identificador', 'Cidade', 'Estado', 'País'], dtype='object', index=['Identificador'])
+#creates a list to store the header of the csv files with identifiers and education
+historicosHeader = ['Identificador', 
+                        'ENSINO-MEDIO-SEGUNDO-GRAU', 'Instituição'.encode('utf-8').decode('utf-8'),'Início'.encode('utf-8').decode('utf-8'), 'Fim',
+                        'CURSO-TECNICO-PROFISSIONALIZANTE',  'Instituição'.encode('utf-8').decode('utf-8'),'Início'.encode('utf-8').decode('utf-8'), 'Fim', 
+                        'GRADUACAO',  'Instituição'.encode('utf-8').decode('utf-8'),'Início'.encode('utf-8').decode('utf-8'), 'Fim', 
+                        'ESPECIALIZACAO',  'Instituição'.encode('utf-8').decode('utf-8'),'Início'.encode('utf-8').decode('utf-8'), 'Fim', 
+                        'MESTRADO',  'Instituição'.encode('utf-8').decode('utf-8'),'Início'.encode('utf-8').decode('utf-8'), 'Fim', 
+                        'DOUTORADO',  'Instituição'.encode('utf-8').decode('utf-8'),'Início'.encode('utf-8').decode('utf-8'), 'Fim']
 
+#starts the csv files with identifiers and education
+historicos_file = open('historicos.csv', 'w', encoding='utf-8', newline='')
+historicos_handle = csv.writer(historicos_file, delimiter='\t')
+historicos_handle.writerow(historicosHeader)
+
+#starts the csv files with identifiers and education in the university
+historicoUniversidade_file = open('historicosUniversidade.csv', 'w', encoding='utf-8', newline='')
+historicosUniversidade_handle = csv.writer(historicoUniversidade_file, delimiter='\t')
+historicosUniversidade_handle.writerow(historicosHeader)
+
+#starts the csv files with identifiers and birth places
+localNascimento_file = open('localNascimento.csv', 'w', encoding='utf-8', newline='')
+localNascimento_handle = csv.writer(localNascimento_file, delimiter='\t')
+localNascimento_handle.writerow(['Identificador', 'Cidade', 'Estado', 'País'.encode('utf-8').decode('utf-8')])
+
+#creates a list to store the birth places coordinates
 coordenadasNascimento = []
-
-#creates a dataframe to store indentifiers and education
-historicos = pd.DataFrame(columns=['Identificador', 
-                                   'ENSINO-MEDIO-SEGUNDO-GRAU', 'Instituição','Início', 'Fim',
-                                   'CURSO-TECNICO-PROFISSIONALIZANTE',  'Instituição','Início', 'Fim', 
-                                   'GRADUACAO',  'Instituição','Início', 'Fim', 
-                                   'ESPECIALIZACAO',  'Instituição','Início', 'Fim', 
-                                   'MESTRADO',  'Instituição','Início', 'Fim', 
-                                   'DOUTORADO',  'Instituição','Início', 'Fim'],dtype='object', index=['Identificador'])
-# creates a series to store the current identifier and education
-historico = pd.Series(index=historicos.columns, dtype='object')
-
-# creates a dataframe to store indentifiers and education in the university
-historicosUniversidade = pd.DataFrame(columns=historicos.columns, dtype='object', index=['Identificador'])
 
 geolocator = Nominatim(user_agent="103962022")
 # %%
@@ -48,7 +57,7 @@ for curriculo in curriculos:
             content = arquivo_handle.read()
             if curriculo.split('.')[-1] == 'zip':
                 if type(content) is type(str):
-                    content = content.encode('utf-8')
+                    content = content
                 zip_memory = io.BytesIO(content)
                 zip_data = zipfile.ZipFile(zip_memory)
                 xml = b''
@@ -58,14 +67,18 @@ for curriculo in curriculos:
 
             #create a tree from the xml
             root = etree.XML(content)
+
+            #get the identifier and the education
             identificador = root.xpath('string(/CURRICULO-VITAE/@NUMERO-IDENTIFICADOR)')
             formacoes = root.xpath('/CURRICULO-VITAE/DADOS-GERAIS/FORMACAO-ACADEMICA-TITULACAO/*')
 
             #reset the check variable and the series
             universityFound = False
+            historico = pd.Series(index=historicosHeader, dtype='object')
+            historicoUniversidade = pd.Series(index=historicosHeader, dtype='object')
             historico.iloc[:] = None
-            #define the identifier in the series
-            historico['Identificador'] = identificador
+            historicoUniversidade.iloc[:] = None
+
             for formacao in formacoes:
                 #get the index of the current course
                 indexFormacao = historico.index.get_loc(formacao.tag)
@@ -73,35 +86,43 @@ for curriculo in curriculos:
                 if formacao.xpath('string(./@NOME-INSTITUICAO)') == 'Centro Federal de Educação Tecnológica de Minas Gerais':
                     universityFound = True
                     #add the course to the university dataframe
-                    historicosUniversidade.loc[identificador, 'Identificador'] = identificador
-                    historicosUniversidade.iloc[historicosUniversidade.index.get_loc(identificador), indexFormacao:indexFormacao+4] = formacao.xpath('string(./@NOME-CURSO)'), formacao.xpath('string(./@NOME-INSTITUICAO)'), formacao.xpath('string(./@ANO-DE-INICIO)'), formacao.xpath('string(./@ANO-DE-CONCLUSAO)')
+                    historicoUniversidade.iloc[indexFormacao:indexFormacao+4] = formacao.xpath('string(./@NOME-CURSO)'), formacao.xpath('string(./@NOME-INSTITUICAO)'), formacao.xpath('string(./@ANO-DE-INICIO)'), formacao.xpath('string(./@ANO-DE-CONCLUSAO)')
                     #add the course to the general dataframe
                     historico.iloc[indexFormacao:indexFormacao+4] = formacao.xpath('string(./@NOME-CURSO)'), formacao.xpath('string(./@NOME-INSTITUICAO)'), formacao.xpath('string(./@ANO-DE-INICIO)'), formacao.xpath('string(./@ANO-DE-CONCLUSAO)')
                 #if isn't the university, check if the course is already in the series
                 elif pd.isnull(historico.iloc[indexFormacao]):
                     historico.iloc[indexFormacao:indexFormacao+4] = formacao.xpath('string(./@NOME-CURSO)'), formacao.xpath('string(./@NOME-INSTITUICAO)'), formacao.xpath('string(./@ANO-DE-INICIO)'), formacao.xpath('string(./@ANO-DE-CONCLUSAO)')
 
-            #if the university was found, add the series to the dataframe
+            #if the university was found, add the series to the file
             if universityFound:
-                historicos.loc[identificador] = historico
-                localNascimento.loc[identificador] = identificador, root.xpath('string(/CURRICULO-VITAE/DADOS-GERAIS/@CIDADE-NASCIMENTO)'), root.xpath('string(/CURRICULO-VITAE/DADOS-GERAIS/@UF-NASCIMENTO)'), root.xpath('string(/CURRICULO-VITAE/DADOS-GERAIS/@PAIS-DE-NASCIMENTO)')
+                historico = historico.to_list()
+                historico[0:0] = [identificador]
+                historicos_handle.writerow(historico)
+
+                historicoUniversidade = historicoUniversidade.to_list()
+                historicoUniversidade[0:0] = [identificador]
+                historicosUniversidade_handle.writerow(historicoUniversidade)
+
+                localNascimento_handle.writerow([identificador, root.xpath('string(/CURRICULO-VITAE/DADOS-GERAIS/@CIDADE-NASCIMENTO)'), root.xpath('string(/CURRICULO-VITAE/DADOS-GERAIS/@UF-NASCIMENTO)'), root.xpath('string(/CURRICULO-VITAE/DADOS-GERAIS/@PAIS-DE-NASCIMENTO)')])
 
         except:
             pass
+
     cont += 1
+
+#close the files
+localNascimento_file.close()
+historicos_file.close()
+historicoUniversidade_file.close()
+
 # %%
-historicos.drop(index='Identificador', inplace=True)
-historicos.to_csv('historicos.csv', sep='\t', encoding='utf-8', index=False)
-
-historicosUniversidade.drop(index='Identificador', inplace=True)
-historicosUniversidade.to_csv('historicosUniversidade.csv', sep='\t', encoding='utf-8', index=False)
-
-localNascimento.drop(index='Identificador', inplace=True)
-localNascimento.to_csv('localNascimento.csv', sep='\t', encoding='utf-8', index=False)
-
-contagemNascimento = localNascimento.value_counts(['Cidade', 'Estado']).to_frame('Quantidade')
+#count the birth places
+contagemNascimento = pd.read_csv('localNascimento.csv', sep='\t')
+contagemNascimento = contagemNascimento.value_counts(['Cidade', 'Estado']).to_frame('Quantidade')
 contagemNascimento.to_csv('contagemNascimento.csv', sep='\t', encoding='utf-8')
+
 # %%
+#locate the birth places coordinates and register their occurrences number
 for local in contagemNascimento.index:
     try:
         geolocalNascimento = geolocator.geocode(local[0] + ', ' + local[1])
@@ -113,8 +134,12 @@ for local in contagemNascimento.index:
         pass
 
 # %%
+#generate the map
 baseMap = folium.Map(width='100%', 
                     height='100%', 
                     location=[-15.77972, -47.92972], 
                     zoom_start=4)
 HeatMap(coordenadasNascimento, radius=12).add_to(baseMap)
+
+#save the map in a html file
+baseMap.save('mapa.html')
